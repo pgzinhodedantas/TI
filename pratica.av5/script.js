dec36e37df6c3b4ebb14d58407dc9a1ca03c2982
pgzinhodedantas <gringohlegal@gmail.com>
2025-09-30 15:35:32 -0300

    Add files via upload

---
+++ pratica.av5/script.js
@@ -0,0 +1,18 @@
+//arrays -> vetores, matrizes
+//estrutura de dados para armazenar um conjunto de elementos
+
+let frutas = ["banana","maçã","melancia"]; //arrrays tamanho 3
+console.log(frutas);
+console.log(frutas[1]);
+
+let numero = new Array(10,2,6,7);
+console.log(numero);
+
+let arraysvario = [];
+console,log(arraysvario);
+arraysvario[0] = "Paulo";
+console.log(arraysvario);
+
+//tamanho do array -> length
+let dados = [32, 4,80,6]; //tamanho 5 -> length 5
+console.log(dados.length);
